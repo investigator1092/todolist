d2ebf3cbab084e996614db05cb8c5ec1f03086fb
investigator1092 <astro.romelda@gmail.com>
2020-02-18 18:30:11 +0900

    誤字修正

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Suppoert\Facades\Schema;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
